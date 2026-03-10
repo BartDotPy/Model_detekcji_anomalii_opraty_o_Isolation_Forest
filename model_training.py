1eed16cde987a4ae594bbc86b1f2a6c82cda4340
BrtKST <b.kostuj1011@gmail.com>
2026-03-10 10:39:06 +0100

    Dodanie wykresu złożoności ścieżek
    Im mniejsza złożoność ścieżek, tym większe prawdopodobieństwo, że dana próbka to anomalia (łatwiej ją "wyciągnąć" z badanego zbioru)

--- model_training.py
+++ model_training.py
@@ -20,13 +20,27 @@ df['anomaly_score'] = model.fit_predict(X)
 norma = df[df['anomaly_score'] == 1]
 anomalie = df[df['anomaly_score'] == -1]
 
-#rysowanie
-plt.figure(figsize=(12,6))
-plt.plot(df['timestamp'], df['cpu_usage'], color = 'gray')
-plt.scatter(norma['timestamp'], norma['cpu_usage'], color = 'blue', alpha = 0.3)
-plt.scatter(anomalie['timestamp'],  anomalie['cpu_usage'], color = 'red', alpha = 0.3)
-plt.title('Predykcja modelu - anomalnie w obciążeniu CPU - Isolation Forest')
-plt.xlabel('Czas')
-plt.ylabel('Obciążenie CPU [%]')
-plt.legend()
+#reprezentacja ścieżek w drzewie (im więcej tym bardziej skomplikowane rozdzieleniu próbki od pozostałych)
+#im mniejsza złożoność rozdzielenia tym większa szanse że próbka jest anomalią
+df['path_score'] = model.decision_function(X)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
+
+#Predykcja anomalii CPU
+ax1.plot(df['timestamp'], df['cpu_usage'], color='gray', alpha=0.5, label='Sygnał CPU')
+ax1.scatter(norma['timestamp'], norma['cpu_usage'], color='blue', s=10, alpha=0.3, label='Norma')
+ax1.scatter(anomalie['timestamp'], anomalie['cpu_usage'], color='red', s=30, label='Anomalia')
+ax1.set_title('Predykcja modelu - Isolation Forest')
+ax1.set_ylabel('Obciążenie CPU [%]')
+ax1.legend()
+
+#Złożoność ścieżek
+ax2.plot(df['timestamp'], df['path_score'], label='Anomaly Score (Głębokość ścieżki)', color='orange')
+ax2.fill_between(df['timestamp'], df['path_score'], 0, where=(df['path_score'] < 0), color='red', alpha=0.3)
+ax2.set_ylabel('Decision Score')
+ax2.set_xlabel('Czas')
+ax2.axhline(0, color='black', lw=1, ls='--') # Linia pomocnicza "0"
+ax2.legend()
+
+plt.tight_layout() # Automatyczne dopasowanie marginesów
 plt.show()
